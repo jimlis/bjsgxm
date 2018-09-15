656e173b465005af75471316ac664da2262ffceb
shenhua <634738651@qq.com>
2018-09-15 14:44:18 +0800

    新增文件管理模块

--- src/main/java/com/ifast/api/controller/ApiFileController.java
+++ src/main/java/com/ifast/api/controller/ApiFileController.java
@@ -8,11 +8,12 @@ import com.ifast.common.utils.Result;
 import com.ifast.common.utils.View;
 import com.ifast.oss.domain.FileDO;
 import com.ifast.oss.service.FileService;
+import io.swagger.annotations.*;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * <pre>
@@ -28,8 +29,13 @@ public class ApiFileController  extends ApiBaseController {
 
     @Log("查询文档附件列表")
     @ResponseBody
-    @GetMapping("/wdlist")
+    @PostMapping("/wdlist")
     @JsonView(View.WdbApp.class)
+    @ApiOperation(value="根据文件类型获取文档附件列表",httpMethod="POST")
+    @ApiImplicitParams(@ApiImplicitParam(name="type",paramType="string",required=false,value = "文件类型"))
+    @ApiResponses({@ApiResponse(code=0,message="操作成功",response=List.class),
+            @ApiResponse(code=1,message="操作失败",response=List.class)})
+    @RequiresAuthentication
     public Result<Page<FileDO>> list(FileDO fileDO) {
         // 查询列表数据
         Page<FileDO> page = fileService.selectPage(getPage(FileDO.class), fileService.convertToEntityWrapper("busType", "bj_wdb", "type", fileDO.getType()));
