170f0f688c6b620b14be4722d4c5fbcf76b7bf77
shenhua <634738651@qq.com>
2018-09-15 15:09:12 +0800

    新增文件管理模块

--- src/main/java/com/ifast/common/utils/View.java
+++ src/main/java/com/ifast/common/utils/View.java
@@ -2,8 +2,13 @@ package com.ifast.common.utils;
 
 /**
  *
- *
  */
 public class View {
    public   interface  WdbApp {}
+
+   public   interface  FileApp {}
+
+   public   interface  GsggListApp {}
+
+   public   interface  GsggDetailsApp {}
 }
